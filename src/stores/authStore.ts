324533bedfa384784b3e3cdde3a1be89c62b0d10
Martine R Karlsen <martinekar08@gmail.com>
2026-04-19 18:16:43 +0200

    Update useAuthStore
    - Keep user logged in on refresh

--- src/stores/authStore.ts
+++ src/stores/authStore.ts
@@ -2,16 +2,16 @@ import { create } from "zustand";
 import type { AuthStore } from "../interfaces/authstore";
 
 /**
- * Create Zustand store for managing authentication state
+ * Zustand store for managing authentication state
  *
  * - Stores user, token, and apiKey
  * - login: saves data to localStorage and updates state.
  * - logout: clear data from localStorage and resets state.
  */
 export const useAuthStore = create<AuthStore>((set) => ({
-  user: null,
-  token: null,
-  apiKey: null,
+  user: JSON.parse(localStorage.getItem("user") ?? "null"),
+  token: localStorage.getItem("token"),
+  apiKey: localStorage.getItem("apiKey"),
 
   login: (user, token, apiKey) => {
     localStorage.setItem("user", JSON.stringify(user));
