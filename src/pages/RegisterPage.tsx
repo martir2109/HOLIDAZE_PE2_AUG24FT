a9de3f2fc53b10b7313edf3c71ec39c403a578fa
Martine R Karlsen <martinekar08@gmail.com>
2026-04-19 15:38:29 +0200

    Create RegisterPage

---
+++ src/pages/RegisterPage.tsx
@@ -0,0 +1,9 @@
+import RegisterFrom from "../components/auth/RegisterForm";
+
+export default function RegisterPage() {
+  return (
+    <main className="w-full min-h-screen flex items-center justify-center px-4">
+      <RegisterFrom />
+    </main>
+  );
+}
