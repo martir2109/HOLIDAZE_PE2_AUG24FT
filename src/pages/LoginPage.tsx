6c1b5e20e22cb4861157309c4ba3e9b201de5e19
Martine R Karlsen <martinekar08@gmail.com>
2026-04-19 15:38:14 +0200

    Create LoginPage

---
+++ src/pages/LoginPage.tsx
@@ -0,0 +1,9 @@
+import LoginForm from "../components/auth/LoginForm";
+
+export default function LoginPage() {
+  return (
+    <main className="w-full min-h-screen flex items-center justify-center px-4">
+      <LoginForm />
+    </main>
+  );
+}
