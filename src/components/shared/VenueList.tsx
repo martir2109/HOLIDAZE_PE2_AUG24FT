9fb72decdd8e45fc8232147e9dcfcbfb2e1e8b7b
Martine R Karlsen <martinekar08@gmail.com>
2026-04-23 09:53:06 +0200

    Create VenueList component
    - Displays a list of venues in a responsive grid layout.

---
+++ src/components/shared/VenueList.tsx
@@ -0,0 +1,21 @@
+import type { Venue } from "../../interfaces/venue";
+import VenueCard from "./VenueCard";
+
+interface VenueListProps {
+  venues: Venue[];
+}
+
+/**
+ * Dsiplays a list of venues in a responsive grid layout.
+ *
+ * @returns A grid of VenueCard components, one for each venue.
+ */
+export default function VenueList({ venues }: VenueListProps) {
+  return (
+    <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
+      {venues.map((venue) => (
+        <VenueCard key={venue.id} venue={venue} />
+      ))}
+    </div>
+  );
+}
