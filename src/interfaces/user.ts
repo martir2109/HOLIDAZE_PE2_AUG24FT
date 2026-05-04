336c8ac3855723ff110a6d646981b0d4d1534a2d
Martine R Karlsen <martinekar08@gmail.com>
2026-05-04 18:12:32 +0200

    Update User interface
    - Added venues, bookings and counts to the User interface.
    - Added SingleApiResponse interface

--- src/interfaces/user.ts
+++ src/interfaces/user.ts
@@ -1,8 +1,25 @@
+import type { Venue, Booking } from "./venue";
+
 export interface User {
-  id: string;
   name: string;
   email: string;
-  avatar: { url: string; alt?: string } | null;
-  banner: { url: string; alt?: string } | null;
+  avatar: {
+    url: string;
+    alt: string;
+  };
+  banner: {
+    url: string;
+    alt: string;
+  };
   venueManager: boolean;
+  _count: {
+    venues: number;
+    bookings: number;
+  };
+  venues?: Venue[];
+  bookings?: Booking[];
+}
+
+export interface SingleApiResponse {
+  data: User;
 }
