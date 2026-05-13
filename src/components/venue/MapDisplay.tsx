1a049c5f3c86e4faf76a4580369080044b46dfdd
Martine R Karlsen <martinekar08@gmail.com>
2026-05-13 19:18:05 +0200

    Hide map when address and city is missing

--- src/components/venue/MapDisplay.tsx
+++ src/components/venue/MapDisplay.tsx
@@ -7,25 +7,27 @@ interface MapDisplayProps {
 /**
  * Shows a Google Maps embed for a venue's address.
  *
- * @param venue - The venue to show the map.
+ * @param venue - The venue whose show the map.
  * @returns A map iframe with the venue's location.
  */
 export default function MapDisplay({ venue }: MapDisplayProps) {
   return (
     <>
-      <div className="flex flex-col gap-2">
-        <h2 className="text-h5 font-bold">Map</h2>
-        <iframe
-          title="Venue location"
-          width="100%"
-          height="300"
-          loading="lazy"
-          className="rounded-md border border-medium-dark-grey"
-          src={`https://www.google.com/maps?q=${encodeURIComponent(
-            `${venue.location.address}, ${venue.location.city}, ${venue.location.zip}, ${venue.location.country}`,
-          )}&z=15&output=embed`}
-        />
-      </div>
+      {(venue.location.address || venue.location.city) && (
+        <div className="flex flex-col gap-2">
+          <h2 className="text-h5 font-bold">Map</h2>
+          <iframe
+            title="Venue location"
+            width="100%"
+            height="300"
+            loading="lazy"
+            className="rounded-md border border-medium-dark-grey"
+            src={`https://www.google.com/maps?q=${encodeURIComponent(
+              `${venue.location.address}, ${venue.location.city}, ${venue.location.zip}, ${venue.location.country}`,
+            )}&z=15&output=embed`}
+          />
+        </div>
+      )}
     </>
   );
 }
